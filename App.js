0a4c4616acaae37598aa3884d1f9992e48f28522
joel-alvarado <joel.alvarado2@upr.edu>
2024-05-01 22:31:33 -0400

    Add countdown timer and start/stop functionality

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 
 export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const remainingTimeFormatted = ({ remainingTime }) => {
+    const minutes = Math.floor(remainingTime / 60)
+    const seconds = remainingTime % 60
+    // If seconds is less than 10, add a leading zero
+    return seconds < 10 ? `${minutes}:0${seconds}` : `${minutes}:${seconds}`;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.mainContainer}>
+      <View style={styles.mainContainer}>
+        <View style={{ marginBottom: 20 }}>
+          <CountdownCircleTimer
+            isPlaying={isPlaying}
+            duration={1500}
+          >
+            {({ remainingTime, color }) => (
+              <Text style={{ color, fontSize: 40 }}>
+                {remainingTimeFormatted({ remainingTime })}
+              </Text>
+            )}
+          </CountdownCircleTimer>
+          <StatusBar style="auto" />
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Pressable onPress={() => setIsPlaying(!isPlaying)} style={styles.timerButton}>
+            <Text style={isPlaying ? styles.stopButton : styles.startButton}> {isPlaying ? 'Stop' : 'Start'}</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  startButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    fontSize: 20,
+  },
+  stopButton: {
+    backgroundColor: 'red',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    fontSize: 20,
+  },
+
 });
